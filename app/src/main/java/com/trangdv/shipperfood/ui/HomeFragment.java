012ec3cd86e146be7d848195c48a0725f283b639
Your Name <itcamxuc11@gmail.com>
2020-08-01 17:25:04 +0700

    first commit

---
+++ app/src/main/java/com/trangdv/shipperfood/ui/HomeFragment.java
@@ -0,0 +1,227 @@
+package com.trangdv.shipperfood.ui;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
+import android.os.Looper;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
+import com.trangdv.shipperfood.R;
+import com.trangdv.shipperfood.common.Common;
+import com.trangdv.shipperfood.model.Request;
+import com.trangdv.shipperfood.model.Token;
+import com.trangdv.shipperfood.viewholder.OrderViewHolder;
+
+public class HomeFragment extends Fragment {
+    private static final String TAG = "HomeFragment";
+
+    FusedLocationProviderClient fusedLocationProviderClient;
+    LocationCallback locationCallback;
+    LocationRequest locationRequest;
+    Location mLastLocation;
+
+    RecyclerView recyclerView;
+    RecyclerView.LayoutManager layoutManager;
+
+    FirebaseDatabase database;
+    DatabaseReference shipperOrders;
+
+    FirebaseRecyclerAdapter<Request, OrderViewHolder> adapter;
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        database = FirebaseDatabase.getInstance();
+        shipperOrders = database.getReference(Common.ORDER_NEED_SHIP_TABLE);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        recyclerView = view.findViewById(R.id.recycler_orders);
+        layoutManager = new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(layoutManager);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // token
+        FirebaseInstanceId.getInstance().getInstanceId().addOnSuccessListener( getActivity(),  new OnSuccessListener<InstanceIdResult>() {
+            @Override
+            public void onSuccess(InstanceIdResult instanceIdResult) {
+                String newToken = instanceIdResult.getToken();
+                Log.e("newToken",newToken);
+
+                updateTokenShipper(newToken);
+            }
+        });
+
+        //check permission
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.CALL_PHONE
+            }, Common.REQUEST_CODE);
+        } else {
+            buildLocationRequest();
+            buildLocationCallBack();
+
+            fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getActivity());
+            fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, Looper.myLooper());
+        }
+
+        // load data
+        loadAllOrderNeedShip(Common.currentShipper.getPhone());
+
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        loadAllOrderNeedShip(Common.currentShipper.getPhone());
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case Common.REQUEST_CODE: {
+                if (grantResults.length > 0) {
+                    if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                        buildLocationRequest();
+                        buildLocationCallBack();
+
+                        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getActivity());
+                        if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+                            return;
+                        }
+                        fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, Looper.myLooper());
+                    }
+                    else
+                    {
+                        Toast.makeText(getActivity(), "You Should Assign Permission!", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+            break;
+            default:
+                break;
+        }
+    }
+
+    private void buildLocationRequest() {
+        locationRequest = new LocationRequest();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setSmallestDisplacement(10f);
+        locationRequest.setInterval(5000);
+        locationRequest.setFastestInterval(3000);
+    }
+
+    private void buildLocationCallBack() {
+        locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                super.onLocationResult(locationResult);
+                mLastLocation = locationResult.getLastLocation();
+                /*Toast.makeText(getContext(), new StringBuffer("")
+                        .append(mLastLocation.getLatitude())
+                        .append("/")
+                        .append(mLastLocation.getLongitude())
+                        .toString(), Toast.LENGTH_LONG).show();*/
+            }
+        };
+    }
+
+    private void updateTokenShipper(String token) {
+
+        DatabaseReference tokens = database.getReference("Tokens");
+        Token data = new Token(token, false);
+        tokens.child(Common.currentShipper.getPhone()).setValue(data);
+    }
+
+    private void loadAllOrderNeedShip(String phone) {
+
+        DatabaseReference orderInChildofShipper = shipperOrders.child(phone);
+        FirebaseRecyclerOptions<Request> listOrders = new FirebaseRecyclerOptions.Builder<Request>()
+                .setQuery(orderInChildofShipper, Request.class)
+                .build();
+
+        adapter = new FirebaseRecyclerAdapter<Request, OrderViewHolder>(listOrders) {
+            @Override
+            protected void onBindViewHolder(@NonNull OrderViewHolder viewHolder, final int position, @NonNull final Request model) {
+                viewHolder.txtOrderId.setText(adapter.getRef(position).getKey());
+                viewHolder.txtOrderPhone.setText(model.getPhone());
+                viewHolder.txtOrderAddress.setText(model.getAddress());
+                viewHolder.txtOrderStatus.setText(Common.convertCodeToStatus(model.getStatus()));
+                viewHolder.txtOrderDate.setText(Common.getDate(Long.parseLong(adapter.getRef(position).getKey())));
+                viewHolder.txtOrderName.setText(model.getName());
+                viewHolder.txtOrderPrice.setText(model.getTotal());
+
+                viewHolder.btnShipping.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Common.createShippingOrder(adapter.getRef(position).getKey(),
+                                Common.currentShipper.getPhone(),
+                                mLastLocation);
+                        Common.currentRequest = model;
+                        Common.currentKey = adapter.getRef(position).getKey();
+
+                        startActivity(new Intent(getActivity(), TrackingOrder.class));
+                        //Toast.makeText(getContext(), "ok", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+
+            @NonNull
+            @Override
+            public OrderViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View itemView = LayoutInflater.from(parent.getContext())
+                        .inflate(R.layout.order_view_layout, parent, false);
+                return new OrderViewHolder(itemView);
+            }
+        };
+
+        adapter.startListening();
+        adapter.notifyDataSetChanged();
+        recyclerView.setAdapter(adapter);
+    }
+}
---
+++ app/src/main/java/com/trangdv/shipperfood/ui/TrackingOrder.java
@@ -0,0 +1,262 @@
+package com.trangdv.shipperfood.ui;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.location.Location;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.res.ResourcesCompat;
+import androidx.fragment.app.FragmentActivity;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.trangdv.shipperfood.R;
+import com.trangdv.shipperfood.common.Common;
+
+public class TrackingOrder extends FragmentActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, com.google.android.gms.location.LocationListener {
+
+    private GoogleMap mMap;
+    private static final String TAG = "TrackingOrder";
+    Location mLastLocation;
+
+    private CameraPosition mCameraPosition;
+
+    private final static int PLAY_SERVICES_RESOLUTION_REQUEST = 1000;
+    private final static int LOCATION_PERMISSION_REQUEST = 1001;
+    private static final String KEY_CAMERA_POSITION = "camera_position";
+    private static final String KEY_LOCATION = "location";
+    private static final int DEFAULT_ZOOM = 15;
+    private final LatLng mDefaultLocation = new LatLng(-33.8523341, 151.2106085);
+
+    private GoogleApiClient mGoogleApiClient;
+    private LocationRequest mLocationRequest;
+
+    private static int UPDATE_INTERVAL = 1000;
+    private static int FASTEST_INTERVAL = 5000;
+    private static int DISPLACEMENT = 10;
+
+
+    private FusedLocationProviderClient mFusedLocationProviderClient ;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tracking_order);
+
+        if (savedInstanceState != null) {
+            mLastLocation = savedInstanceState.getParcelable(KEY_LOCATION);
+            mCameraPosition = savedInstanceState.getParcelable(KEY_CAMERA_POSITION);
+        }
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestRuntimePermission();
+        } else {
+            if (checkPlayServices()) {
+                buildingGoogleApiClient();
+                createLocationRequest();
+            }
+        }
+
+        mFusedLocationProviderClient  = LocationServices.getFusedLocationProviderClient(this);
+
+        displayLocation();
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    private void displayLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestRuntimePermission();
+        } else {
+
+            Task locationResult = mFusedLocationProviderClient.getLastLocation();
+            locationResult.addOnCompleteListener(this, new OnCompleteListener() {
+                @Override
+                public void onComplete(@NonNull Task task) {
+                    if (task.isSuccessful()) {
+
+                        mLastLocation = (Location) task.getResult();
+                        /*mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                                new LatLng(mLastLocation.getLatitude(),
+                                        mLastLocation.getLongitude()), DEFAULT_ZOOM));*/
+
+                        LatLng yourLocation = new LatLng(mLastLocation.getLatitude(), mLastLocation.getLongitude());
+                        //
+                        Drawable myDrawable = ResourcesCompat.getDrawable(getResources(), R.drawable.my_location, null);
+                        Bitmap bitmap = ((BitmapDrawable) myDrawable).getBitmap();
+                        bitmap = Common.scaleBitmap(bitmap, 70, 70);
+                        MarkerOptions marker = new MarkerOptions().icon(BitmapDescriptorFactory.fromBitmap(bitmap))
+                                .title("Order of " + Common.currentShipper.getPhone())
+                                .position(yourLocation);
+                        mMap.addMarker(marker);
+
+
+                        String latitude = Common.currentRequest.getLatitude();
+                        String longitude = Common.currentRequest.getLongitude();
+                        LatLng orderLocation = new LatLng(Double.parseDouble(latitude), Double.parseDouble(longitude));
+                        //
+                        myDrawable = ResourcesCompat.getDrawable(getResources(), R.drawable.box, null);
+                        bitmap = ((BitmapDrawable) myDrawable).getBitmap();
+                        bitmap = Common.scaleBitmap(bitmap, 70, 70);
+                        MarkerOptions markerOrder = new MarkerOptions().icon(BitmapDescriptorFactory.fromBitmap(bitmap))
+                                .title("Order of " + Common.currentRequest.getPhone())
+                                .position(orderLocation);
+                        mMap.addMarker(markerOrder);
+                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                                orderLocation, DEFAULT_ZOOM));
+
+                    } else {
+                        Log.d(TAG, "Current location is null. Using defaults.");
+                        Log.e(TAG, "Exception: %s", task.getException());
+                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(mDefaultLocation, DEFAULT_ZOOM));
+                        mMap.getUiSettings().setMyLocationButtonEnabled(false);
+                    }
+                }
+            });
+        }
+    }
+
+
+    private void createLocationRequest() {
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(UPDATE_INTERVAL);
+        mLocationRequest.setFastestInterval(FASTEST_INTERVAL);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setSmallestDisplacement(DISPLACEMENT);
+    }
+
+    protected synchronized void buildingGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API).build();
+        mGoogleApiClient.connect();
+
+    }
+
+    private boolean checkPlayServices() {
+        int resultCode = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(this);
+        if (resultCode != ConnectionResult.SUCCESS) {
+            if (GoogleApiAvailability.getInstance().isUserResolvableError(resultCode)) {
+                GoogleApiAvailability.getInstance().getErrorDialog(this, resultCode, PLAY_SERVICES_RESOLUTION_REQUEST);
+            } else {
+                Toast.makeText(this, "This device does not support Maps!!", Toast.LENGTH_LONG).show();
+            }
+        }
+        return true;
+    }
+
+    private void requestRuntimePermission() {
+        ActivityCompat.requestPermissions(this, new String[]{
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+        }, LOCATION_PERMISSION_REQUEST);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode) {
+            case LOCATION_PERMISSION_REQUEST:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (checkPlayServices()) {
+                        buildingGoogleApiClient();
+                        createLocationRequest();
+                        displayLocation();
+                    }
+
+                }
+                break;
+
+        }
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        mLastLocation = location;
+        displayLocation();
+
+    }
+
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        displayLocation();
+        startLocationUpdates();
+
+    }
+
+    //check
+    private void startLocationUpdates() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        mGoogleApiClient.connect();
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        checkPlayServices();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (mGoogleApiClient != null) {
+            mGoogleApiClient.connect();
+        }
+    }
+
+}
